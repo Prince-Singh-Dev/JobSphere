fc0ef59471cdeb4fc5fd9003d2f5705c31c4cf1c
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-05-15 16:31:37 +0000

    completed the navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AppContextProvider } from './context/AppContext.jsx'
-import {ClerkProvider} from '@clerk/react'
+import {ClerkProvider} from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
