e94f96a9c31763523ee6c86e0d4d496fcae1ba10
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-06-07 09:08:49 +0000

    Created more jobs section in apply page

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -6,6 +6,7 @@ import Navbar from '../components/Navbar'
 import { assets } from '../assets/assets'
 import kconvert from 'k-convert';
 import moment from 'moment';
+import JobCard from '../components/JobCard'
 
 const ApplyJob = () => {
 
@@ -65,12 +66,21 @@ const ApplyJob = () => {
 
           </div>
 
-      <div className='flex flex-col lg:flew-row justify-between items-start'>
+      <div className='flex flex-col lg:flex-row justify-between items-start'>
         <div className='w-full lg:w-2/3'>
           <h2 className='font-bold text-2xl mb-4'>Job description</h2>
           <div className='rich-text' dangerouslySetInnerHTML={{__html:JobData.description}}></div>
           <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>Apply Now</button>
         </div>
+
+        {/* Right section for showing more related jobs */}
+        <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5'>
+            <h2>More jobs from {JobData.companyId.name}</h2>
+            {jobs.filter( job => job._id !== JobData._id  && job.companyId._id === JobData.companyId._id)
+            .filter( job => true).slice(0,4)
+            .map((job,index) => <JobCard key={index} job={job}/>)}
+        </div>
+
       </div>
 
         </div>
