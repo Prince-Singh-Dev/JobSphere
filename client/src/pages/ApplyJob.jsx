491a83975f23bd36331bc7084fb2daf90348d11e
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-06-07 09:10:45 +0000

    Completed the ApplyJobs Page

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -7,6 +7,7 @@ import { assets } from '../assets/assets'
 import kconvert from 'k-convert';
 import moment from 'moment';
 import JobCard from '../components/JobCard'
+import Footer from '../components/Footer'
 
 const ApplyJob = () => {
 
@@ -85,6 +86,9 @@ const ApplyJob = () => {
 
         </div>
       </div>
+      
+      <Footer/>
+
     </>
   ) : (
     <Loading/>
