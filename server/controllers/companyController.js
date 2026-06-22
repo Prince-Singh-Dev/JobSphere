4d4d504d6d6438ca1bf08971006c2c28d61f5d28
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-06-22 01:00:08 +0000

    COnfigured Cloudinary and Setup the register route for company

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -1,7 +1,24 @@
+import Company from "../models/Company.js";
+import bcrypt from 'bcrypt'
 
 // Register a new company
 export const registerCompany = async (req,res) => {
-    
+    const {name,email,password} = req.body
+    const imageFile = req.file;
+    if (!name || !email || !password || !imageFile) {
+        return res.json({success:false , message:"Missing Details"})
+    }
+    try {
+        const companyExists = await Company.findOne({email})
+        if (companyExists) {
+            return res.json({success:false,message:"Company Already Registered"})
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password,salt)
+        
+    } catch (error) {
+        
+    }
 }
 
 
