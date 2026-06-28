69fd4125b0f75999dbf83d98f502a8b04ba1fa9b
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-06-28 03:05:05 +0000

    created the logic of user resume update using clerk + cloudinary

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import JobApplication from "../models/JobApplication.js"
 import User from "../models/User.js"
+import { v2 as cloudinary } from "cloudinary"
 
 // Get User data
 export const getUserData = async(req,res) => {
@@ -59,5 +60,17 @@ export const getUserJobApplications = async(req,res) => {
 
 // Update user profile
 export const updateUserResume = async(req,res) =>{
-
+    try {
+        const userId = req.auth.userId
+        const resumeFile = req.resumeFile
+        const userData = await User.findById(userId)
+        if (resumeFile) {
+            const resumeUpload = await cloudinary.uploader.upload(resumeFile.path)
+            userData.resume = resumeUpload.secure_url
+        }
+        await userData.save()
+        return res.json({success:true , message:'Resume Updated'})
+    } catch (error) {
+        res.json({success:false , message:error.message})
+    }
 }
