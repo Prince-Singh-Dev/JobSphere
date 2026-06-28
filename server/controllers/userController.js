bcc89e3a2a0fc2407df1addbbdfb394daaeeb7cf
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-06-28 02:36:15 +0000

    created the getUserData route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,22 @@
+import User from "../models/User.js"
 
 // Get User data
 export const getUserData = async(req,res) => {
-
+    const userId = req.auth.userId
+    try {
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.json({success:false,message:'User not found'})
+        }
+        res.json({success:true,user})
+    } catch (error) {
+        res.json({success:false , message:error.message})
+    }
 }
 
 // Apply for a job
 export const applyForJob = async (req,res) => {
-
+    
 }
 
 // Get user applied applications
