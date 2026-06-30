612f631c196e77ea27fd6a7c32f5d99496e42058
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-06-30 02:37:26 +0000

    completed logout recruiter panel function

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -7,7 +7,16 @@ import { AppContext } from '../context/AppContext'
 const Dashboard = () => {
 
     const navigate = useNavigate()
-    const {companyData} = useContext(AppContext)
+    const {companyData , setCompanyData , setCompanyToken} = useContext(AppContext)
+
+    // function to logout from recruiter panel
+    const logout = () => {
+        setCompanyToken(null)
+        localStorage.removeItem('companyToken')
+        setCompanyData(null)
+        navigate('/')
+    }
+
   return (
     <div className='min-h-screen'>
         {/* Navbar for Recruiter Panel */}
@@ -21,7 +30,7 @@ const Dashboard = () => {
                             <img className='w-8 border rounded-full' src={companyData.image} alt="" />
                             <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
                                 <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
-                                    <li className='py-1 px-2 cursor-pointer pr-2'>Logout</li>
+                                    <li onClick={logout} className='py-1 px-2 cursor-pointer pr-2'>Logout</li>
                                 </ul>
                             </div>
                         </div>
