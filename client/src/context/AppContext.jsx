e10f3f3f27fb96e9eedd7dd22dfd533c82d06c66
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-07-02 02:19:58 +0000

    completed the applied jobs page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -78,6 +78,24 @@ export const AppContextProvider = (props) => {
         }
     }
 
+    // Function to fetch user's applied applications data
+    const fetchUserApplications = async () => {
+        try {
+            const token = await getToken()
+            const {data} = await axios.get(backendUrl+'/api/users/applications',
+                {headers:{Authorization:`Bearer ${token}`}}
+            )
+            if(data.success){
+                setUserApplications(data.applications)
+            }
+            else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     useEffect(() => {
         fetchJobs()
 
@@ -96,6 +114,7 @@ export const AppContextProvider = (props) => {
     useEffect(()=>{
         if (user) {
             fetchUserData()
+            fetchUserApplications()
         }
     },[user])
 
@@ -109,7 +128,8 @@ export const AppContextProvider = (props) => {
         backendUrl , 
         userData , setUserData ,
         userApplications , setUserApplications ,
-        fetchUserData
+        fetchUserData, 
+        fetchUserApplications,
     }
 
     return (<AppContext.Provider value={value}>
